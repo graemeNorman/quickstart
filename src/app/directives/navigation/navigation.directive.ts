5f56dd9a20040eb83ec37c6c2986464885de48f0
graemeNorman <graemenorman@tengroup.com>
2017-02-09 13:19:59 +0000

    Directives folder and initial navigation directive added

---
+++ src/app/directives/navigation/navigation.directive.ts
@@ -0,0 +1,21 @@
+import {Component} from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'mainNavigation',
+    templateUrl: './navigation.template.html'
+})
+
+export class mainNavigation {
+    // forename : string;
+    // surname : string;
+    // email : string;
+    // tel : string;
+
+    constructor(){
+        // this.forename = "Please enter your first name"
+        // this.surname = "Please enter your last name"
+        // this.email = "Please enter an email address"
+        // this.tel = "Please enter a telephone number"
+    };
+}
