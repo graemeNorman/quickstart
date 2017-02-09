ddc9b766e1763c96c2f880eb9c4995deef9a4e21
graemeNorman <graemenorman@tengroup.com>
2017-02-09 13:23:18 +0000

    Routing / Components / Directives all declared in app.module && app.component specifies a proper HTML template through templateUrl rather than an being inline

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,30 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Main App component
 import { AppComponent }  from './app.component';
 
+// Components
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+
+// Directives
+import { mainNavigation } from './directives/navigation/navigation.directive';
+
+// Routing
+import { routing } from "./app.routing";
+
+
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule,
+                  routing
+                ],
+  declarations: [ AppComponent,
+                  HomeComponent,
+                  AboutComponent,
+                  mainNavigation
+                ],
   bootstrap:    [ AppComponent ]
 })
+
 export class AppModule { }
